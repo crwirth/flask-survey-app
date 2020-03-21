a65e6c844f562fa26043f0b91507e00dd89050cb
Cody Wirth <wirthrcody@gmail.com>
2020-03-21 08:24:20 -0400

    finish

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash, jsonify
+from flask import Flask, request, render_template, redirect, flash, jsonify, session
 from surveys import satisfaction_survey as survey
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -8,7 +8,7 @@ app.config['SECRET_KEY'] = "oh-so-secret"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-responses = []
+RESPONSES_KEY = "responses"
 
 @app.route('/')
 def home_page():
@@ -16,4 +16,39 @@ def home_page():
 
 @app.route('/start', methods=["POST"])
 def start_survey():
+
+    session[RESPONSES_KEY] = []
+
     return redirect("/questions/0")
+
+@app.route("/questions/<int:qid>")
+def display_question(qid):
+    responses = session.get(RESPONSES_KEY)
+
+    if (responses is None):
+        return redirect("/")
+    
+    if (len(responses) == len(survey.questions)):
+        return redirect("/complete")
+    
+    if (len(responses) != qid):
+        flash(f"Invalid question id: {qid}.")
+        return redirect(f"/questions/{len(responses)}")
+    
+    question = survey.questions[qid]
+    return render_template("question.html", question_num=qid, question=question)
+
+@app.route("/answer", methods=["POST"])
+def handle_question():
+
+    choice = request.form['answer']
+
+    responses = session[RESPONSES_KEY]
+    responses.append(choice)
+    session[RESPONSES_KEY] = responses
+
+    if (len(responses) == len(survey.questions)):
+        return redirect("/complete")
+
+    else:
+        return redirect(f"/questions/{len(responses)}")
